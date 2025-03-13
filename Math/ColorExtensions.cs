934a289731489e75e4bd01b07db04f387af3d31d
Arseni <arseni.natapov@gmail.com>
2025-03-13 21:43:37 +0300

    Added color extensions

---
+++ Math/ColorExtensions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace BB
+{
+	public static class ColorExtensions
+	{
+		public static bool Approximately(
+			this Color c1,
+			Color c2)
+		=> c1.r.Approximately(c2.r)
+			&& c1.g.Approximately(c2.g)
+			&& c1.b.Approximately(c2.b)
+			&& c1.a.Approximately(c2.a);
+	}
+}
