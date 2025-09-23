a65afcd463c1bee967b04611db4597352a03c7e4
Arseni <arseni.natapov@gmail.com>
2025-09-23 20:29:51 +0300

    Update Direction.cs

--- Unity/Direction.cs
+++ Unity/Direction.cs
@@ -22,5 +22,16 @@ namespace BB
                 return Mathf.Sign(v.z) > 0 ? Direction.Forward : Direction.Back;
             return Mathf.Sign(v.x) > 0 ? Direction.Right : Direction.Left;
         }
+        public static Vector3 ToV3(this Direction dir)
+            => dir switch
+            {
+                Direction.Forward => Vector3.forward,
+                Direction.Back => Vector3.back,
+                Direction.Left => Vector3.left,
+                Direction.Right => Vector3.right,
+                Direction.Up => Vector3.up,
+                Direction.Down => Vector3.down,
+                _ => Vector3.zero
+            };
     }
 }
