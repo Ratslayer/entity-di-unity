41cc1042e52c675530820e5ddccfcd36e25d124a
Arseni <arseni.natapov@gmail.com>
2026-05-23 09:09:18 +0300

    Added BoardValues struct

--- Unity/Di/New/UnityWorldBootstrap.cs
+++ Unity/Di/New/UnityWorldBootstrap.cs
@@ -4,26 +4,15 @@ namespace BB.Di
 {
     public static class UnityWorldBootstrap
     {
-        private static EntityBootstrapSettings _settings;
-
         public static EntityBootstrapSettings Settings
-        {
-            get
-            {
-                if (_settings)
-                    return _settings;
-
-                _settings = Resources.Load<EntityBootstrapSettings>("UnityWorldBootstrapSettings");
-                return _settings;
-            }
-        }
+            => Resources.Load<EntityBootstrapSettings>("UnityWorldBootstrapSettings");
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
         static void Clear()
         {
             if (WorldBootstrap.World is null)
                 return;
-            
+
             Debug.Log("Destroying current world.");
             WorldBootstrap.DestroyWorld();
         }
@@ -33,7 +22,7 @@ namespace BB.Di
         {
             if (WorldBootstrap.World is not null)
                 return;
-            
+
             Debug.Log("Creating runtime world");
             WorldBootstrap.SpawnWorld(Settings._runtimeConfig);
         }
