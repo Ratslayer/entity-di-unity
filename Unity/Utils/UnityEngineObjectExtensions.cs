46fbd8518d18473a11446a49eba92d2bda56faec
Arseni <arseni.natapov@gmail.com>
2026-06-20 12:06:32 +0300

    Added changing climb messages

--- Unity/Utils/UnityEngineObjectExtensions.cs
+++ Unity/Utils/UnityEngineObjectExtensions.cs
@@ -1,19 +1,28 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
 namespace BB
 {
-	public static class UnityEngineObjectExtensions
-	{
-		public static T NullIfDestroyedUnityEngineObject<T>(this T t)
-			where T : class
-			=> t is Object obj && !obj ? null : t;
-		public static bool IsNotNullOrDestroyed<T>(this T t)
-			where T : class
-			=> t.NullIfDestroyedUnityEngineObject() is not null;
-		public static bool IsNullOrDestroyed<T>(this T t)
+    public static class UnityEngineObjectExtensions
+    {
+        public static T NullIfDestroyedUnityEngineObject<T>(this T t)
+            where T : class
+            => t is Object obj && !obj ? null : t;
+
+        public static bool IsNotNullOrDestroyed<T>(this T t)
+            where T : class
+            => t.NullIfDestroyedUnityEngineObject() is not null;
+
+        public static bool IsNullOrDestroyed<T>(this T t)
             where T : class
             => t.NullIfDestroyedUnityEngineObject() is null;
 
-		public static string GetNameSafe(this Object obj)
-			=> obj ? obj.name : "NONE";
-	}
+        public static string GetNameSafe(this Object obj)
+            => obj ? obj.name : "NONE";
+
+        public static IEnumerable<T> WhereNotNullOrDestroyed<T>(this IEnumerable<T> t)
+            where T : class
+            => t.Where(v => v.IsNotNullOrDestroyed());
+    }
 }
